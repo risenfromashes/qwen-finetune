297a8781c796d75a9a9ab36d1682041dc68140c6
System #3 <system3@system3.cse.buet.ac.bd>
2024-12-17 18:17:53 +0600

    stuff

---
+++ quant.py
@@ -0,0 +1,21 @@
+from awq import AutoAWQForCausalLM
+from transformers import AutoTokenizer
+
+model_path = 'qwen2.5-32b-nlgraph/merged/sft'
+quant_path = 'qwen2.5-32b-nlgraph/awq/sft'
+quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+
+# Load model
+model = AutoAWQForCausalLM.from_pretrained(
+    model_path, **{"low_cpu_mem_usage": True, "use_cache": False}
+)
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+# Quantize
+model.quantize(tokenizer, quant_config=quant_config)
+
+# Save quantized model
+model.save_quantized(quant_path)
+tokenizer.save_pretrained(quant_path)
+
+print(f'Model is quantized and saved at "{quant_path}"')
